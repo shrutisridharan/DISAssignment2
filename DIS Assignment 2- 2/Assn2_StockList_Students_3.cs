010c96eccdaaca2ebfc967537e6c2390ebcf81f5
shrutisridharan <shrutis@mail.usf.edu>
2018-09-27 18:22:27 -0400

    i am testing pushing th Test-doc-git-pushing.txt to the GIT server

--- DIS Assignment 2- 2/Assn2_StockList_Students_3.cs
+++ DIS Assignment 2- 2/Assn2_StockList_Students_3.cs
@@ -4,42 +4,43 @@ using System.Text;
 
 namespace Assignment_2
 {
-  public partial class StockList
-  {
-    //param        : NA
-    //summary      : Calculate the value of each node by multiplying holdings with price, and returns the total value
-    //return       : total value
-    //return type  : decimal
-    public decimal Value()
+    public partial class StockList
     {
-      decimal value = 0.0m;
+        //param        : NA
+        //summary      : Calculate the value of each node by multiplying holdings with price, and returns the total value
+        //return       : total value
+        //return type  : decimal
+        public decimal Value()
+        {
+            int var = 0;
+            decimal value = 0.0m;
 
-      // write your implementation here
+            // write your implementation here
 
-      return value;
+            return value;
+        }
+
+        //param  (StockList) listToCompare     : StockList which has to comared for similarity index
+        //summary      : finds the similar number of nodes between two lists
+        //return       : similarty index
+        //return type  : int
+        public int Similarity(StockList listToCompare)
+        {
+            int similarityIndex = 0;
+
+            // write your implementation here
+
+            return similarityIndex;
+        }
+
+        //param        : NA
+        //summary      : Print all the nodes present in the list
+        //return       : NA
+        //return type  : NA
+        public void Print()
+        {
+            // write your implementation here
+
+        }
     }
-
-    //param  (StockList) listToCompare     : StockList which has to comared for similarity index
-    //summary      : finds the similar number of nodes between two lists
-    //return       : similarty index
-    //return type  : int
-    public int Similarity(StockList listToCompare)
-    {
-      int similarityIndex = 0;
-
-      // write your implementation here
-
-      return similarityIndex;
-    }
-
-    //param        : NA
-    //summary      : Print all the nodes present in the list
-    //return       : NA
-    //return type  : NA
-    public void Print()
-    {
-      // write your implementation here
-
-    }
-  }
 }
